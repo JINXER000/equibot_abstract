3c8156903a4ac4823cbd7d7db39e47134e44f9f2
jinxer000 <czp7914@126.com>
2025-07-12 14:21:54 +0800

    clean code

--- equibot/policies/datasets/dmg_dataset.py
+++ equibot/policies/datasets/dmg_dataset.py
@@ -6,10 +6,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
-from tqdm import tqdm
-from collections import namedtuple
-from equibot.policies.utils.constants import qpos_to_eepose
-# from equibot.envs.sim_mobile.utils.transformations import quat2mat
 from equibot.policies.vision.vdgcnn_encoder import VecDGCNN_att_frozen
 from equibot.policies.datasets.effpose_estimation import solve_pairwise_registration, debug_and_save
 from equibot.policies.utils.misc import to_torch, rotate_observation, rotate_vec_grasp, to_tensor, to_np, EQUIBOT_PATH, str_to_ascii_tensor, ascii_tensor_to_str, get_skill_names, compose_transformation, centralize_downsample, centralize_grasp
