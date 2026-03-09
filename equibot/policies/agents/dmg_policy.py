can run after merge, but wrong result. reset to analyze

equibot/policies/agents/dmg_policy.py
@@ -360,7 +360,7 @@ class DMGPolicy(nn.Module):
 
         return action_dict, eval_metrics
     
-    def pred_unimaual_traj(self, skill_name, agent_obs, gt_batch = None, task_name_batch = None):
+    def pred_unimanual_traj(self, skill_name, agent_obs, gt_batch = None, task_name_batch = None):
         pc_data = agent_obs[f'{skill_name}:pc'].repeat(1, self.obs_horizon, 1, 1)
         batch_size =  pc_data.shape[0]
 
@@ -478,7 +478,7 @@ class DMGPolicy(nn.Module):
             else:
                 pc_data = batch[f'{skill_name}:pc']
                 agent_obs = {f'{skill_name}:pc': pc_data}
-                action_dict, eval_metrics = self.pred_unimaual_traj(skill_name, agent_obs, gt_batch=batch)
+                action_dict, eval_metrics = self.pred_unimanual_traj(skill_name, agent_obs, gt_batch=batch)
             action_dict_all.update(action_dict)
             eval_metrics_all.update(eval_metrics)
 

equibot/policies/agents/per_skill_policy.py
@@ -378,7 +378,7 @@ class EquiSkillPolicy(nn.Module):
 
         return obs_vec
 
-    def pred_unimaual_traj(self, skill_name_batch, agent_obs, gt_batch = None, task_name_batch = None):
+    def pred_unimanual_traj(self, skill_name_batch, agent_obs, gt_batch = None, task_name_batch = None):
         pc_data = agent_obs['pc'].repeat(1, self.obs_horizon, 1, 1)
         batch_size =  pc_data.shape[0]
 
@@ -513,7 +513,7 @@ class EquiSkillPolicy(nn.Module):
         # if 'in_hand_pc' in batch:
         #     agent_obs['in_hand_pc'] = batch['in_hand_pc']
             # agent_obs['in_hand_mask'] = batch['in_hand_mask']
-        action_dict, eval_metrics = self.pred_unimaual_traj(skill_name_batch, agent_obs, gt_batch=batch, task_name_batch=task_name_batch)
+        action_dict, eval_metrics = self.pred_unimanual_traj(skill_name_batch, agent_obs, gt_batch=batch, task_name_batch=task_name_batch)
         action_dict_all.update(action_dict)
         eval_metrics_all.update(eval_metrics)
 